56dc5ed36875d1822a1b894427e36b1c301e5d58
halilesmer <taximinator@gmail.com>
2026-08-15 00:19:47 +0200

    fix(scripts): set default TEST environment and port 8501 in run_all_services

--- scripts/run_all_services.bat
+++ scripts/run_all_services.bat
@@ -1,6 +1,6 @@
 :: Dosya: scripts/run_all_services.bat
 @echo off
-set ROBOT_ENV=LIVE
-set STREAMLIT_PORT=8502
+set ROBOT_ENV=TEST
+set STREAMLIT_PORT=8501
 cd /d "%~dp0\.."
 python scripts\launcher.py --open-browser
